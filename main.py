e25ae3338bc06af7f5c4bb629233ebfa915a64c6
joeol <joeoliverevans@gmail.com>
2023-11-25 12:01:05 +0000

    added ability to create a random graph and create a graph from an adjacency matrix

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 
@@ -40,7 +42,10 @@ class Graph:
 
         norm_adjacency = np.zeros(self.adjacency.shape)
         for row in range(self.adjacency.shape[0]):
-            norm_adjacency[row, :] = self.adjacency[row, :] / np.sum(self.adjacency[row, :])
+            if np.sum(self.adjacency[row, :]) == 0:
+                norm_adjacency[row, :] = np.zeros(norm_adjacency[row, :].shape)
+            else:
+                norm_adjacency[row, :] = self.adjacency[row, :] / np.sum(self.adjacency[row, :])
 
         e = 1 / len(self.dictionary) * np.ones(len(self.dictionary))
 
@@ -64,8 +69,69 @@ class Graph:
                 break
         return p
 
+    def connectedComponents(self):
+        """
+        Returns the number of connected componentsn and the list of visited connected compoennts
+        :param self:
+        :return: k, components[]
+        """
+        k = int
+        components = []
+        visited = []
+        for v in self.vertices:
+            if v not in visited:
+                nodes = []
+                visited, nodes = depthFirstSearch(v, visited)
+                k += 1
+                components.append(nodes)
+        return k, components
+
+    def depthFirstSearch(self, v, visited):
+        """
+        Performs a depth first search
+        :param v:
+        :param visited:
+        :return:
+        """
+def wattsStrogatz(N, K, p):
+    """
+    Generate a random graph using the Watts-Strogatz method
+    :param N: Graph order
+    :param K: Mean degree (even integer)
+    :param p: rewiring probability
+    :return: A random graph
+    """
+    A = np.zeros((N, N))
+    for i in range(N):
+        for j in range(N):
+            if j < i:
+                if (np.abs(i - j) % (N - 1 - K / 2)) > 0 and (np.abs(i-j) % (N-1-K/2)) <= K/2:
+                    A[i, j], A[j, i] = 1, 1
+    for i in range(N):
+        for j in range(N):
+            if j < i:
+                if A[i, j] == 1 and np.random.rand() < p:
+                    k = int(N * np.random.rand())
+                    while k == i or A[i, k] == 1:
+                        k = int(N * np.random.rand())
+                    A[i, j], A[j, i] = 0, 0
+                    A[i, k], A[k, i] = 1, 1
+    return A
+
+
+def verticesAndEdgesFromAdjacency(A):
+    """
+    Finds edges and vertices from the adjacency matrix
+    :param A: Adjacency matrix
+    :return: (vertices, edges)
+    """
+    vertices = np.arange(A.shape[0])
+    edges = np.argwhere(A == 1)
+    return vertices, edges
+
 
 newGraph = Graph(['A', 'B', 'C', 'D'], [['B', 'A'], ['B', 'C'], ['C', 'A'], ['D', 'A'], ['D', 'B'], ['D', 'C']], True)
+graph2 = Graph(*verticesAndEdgesFromAdjacency(wattsStrogatz(10, 3, 0.5)), True)
 
 print(newGraph.adjacency)
-print(newGraph.PageRank())
+print(graph2.edges)
